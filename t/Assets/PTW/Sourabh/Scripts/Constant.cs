f58cbfb092a85dbb538a4d69ce35846fc13ce361
PTW-I\sourabh.singhai <sourabh.singhai@ptw.com>
2022-09-26 16:21:07 +0530

    Initial Commit

---
+++ t/Assets/PTW/Sourabh/Scripts/Constant.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Constant 
+{
+    public const string vivoxServerAddress = "https://mt1s.www.vivox.com/api2";
+    public const string domain = "mt1s.vivox.com";
+    public const string tokenIssuer = "ptw7337-bo53-dev";
+    public const string tokenKey = "lock742";
+}
